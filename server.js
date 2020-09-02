256e3a3c946ac0ea21e6c2c42f25dc7520d396b5
Shifath Salam <shifathsalam@gmail.com>
2020-09-02 02:06:25 -0700

    worked on user registration

--- server.js
+++ server.js
@@ -6,6 +6,9 @@ const app = express();
 // Connect Database
 connectDB();
 
+// Init Middleware
+app.use(express.json());
+
 // Define Routes
 app.use("/api/users", require("./routes/api/users"));
 app.use("/api/auth", require("./routes/api/auth"));
