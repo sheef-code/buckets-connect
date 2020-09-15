a7c27927119506ee22f01a8b81ec1776360fa577
Shifath Salam <shifathsalam@gmail.com>
2020-09-15 14:35:16 -0700

    created single post component

--- client/src/components/routing/Routes.js
+++ client/src/components/routing/Routes.js
@@ -10,6 +10,7 @@ import AddEducation from "../profile-forms/AddEducation";
 import Profiles from "../profiles/Profiles";
 import Profile from "../profile/Profile";
 import Posts from "../posts/Posts";
+import Post from "../post/Post";
 import PrivateRoute from "../routing/PrivateRoute";
 
 const Routes = (props) => {
@@ -27,6 +28,7 @@ const Routes = (props) => {
         <PrivateRoute exact path="/add-experience" component={AddExperience} />
         <PrivateRoute exact path="/add-education" component={AddEducation} />
         <PrivateRoute exact path="/posts" component={Posts} />
+        <PrivateRoute exact path="/posts/:id" component={Post} />
       </Switch>
     </section>
   );
