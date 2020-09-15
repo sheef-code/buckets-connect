2484a4c638192259d646849c1c6c949e339aa449
Shifath Salam <shifathsalam@gmail.com>
2020-09-14 21:48:28 -0700

    created experience and education section for profile component

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -5,6 +5,8 @@ import { connect } from "react-redux";
 import Spinner from "../layout/Spinner";
 import ProfileTop from "./ProfileTop";
 import ProfileAbout from "./ProfileAbout";
+import ProfileExperience from "./ProfileExperience";
+import ProfileEducation from "./ProfileEducation";
 import { getProfileById } from "../../actions/profile";
 
 const Profile = ({ getProfileById, profile: { profile }, auth, match }) => {
@@ -35,7 +37,12 @@ const Profile = ({ getProfileById, profile: { profile }, auth, match }) => {
               <h2 className="text-primary">Experience</h2>
               {profile.experience.length > 0 ? (
                 <Fragment>
-                  {profile.experience.map((experience) => ({ experience }))}
+                  {profile.experience.map((experience) => (
+                    <ProfileExperience
+                      key={experience._id}
+                      experience={experience}
+                    />
+                  ))}
                 </Fragment>
               ) : (
                 <h4>No experience credentials</h4>
@@ -45,7 +52,12 @@ const Profile = ({ getProfileById, profile: { profile }, auth, match }) => {
               <h2 className="text-primary">Education</h2>
               {profile.education.length > 0 ? (
                 <Fragment>
-                  {profile.education.map((education) => ({ education }))}
+                  {profile.education.map((education) => (
+                    <ProfileEducation
+                      key={education._id}
+                      education={education}
+                    />
+                  ))}
                 </Fragment>
               ) : (
                 <h4>No education credentials</h4>
